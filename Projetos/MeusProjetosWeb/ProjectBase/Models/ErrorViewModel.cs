3d7094da663c34897596d22a723a1ae60b79715d
Andrei Hirata <161364775+gitandreihirata@users.noreply.github.com>
2026-06-17 21:09:37 -0300

    ProjectBase-Part_1, Apostila atualizada do banco de dados,
    ProjectBase-Part_1

---
+++ Projetos/MeusProjetosWeb/ProjectBase/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectBase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- Projetos/MeusProjetosWeb/ProjetoBase/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjetoBase.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
